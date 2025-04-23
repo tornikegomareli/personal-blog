75eaf40bd517b9906d6db085c93c369aa4cbe51a
Tornike Gomareli <24585160+tornikegomareli@users.noreply.github.com>
2025-04-23 14:04:24 +0400

    make first photo as cover

--- app/[slug]/layout.js
+++ app/[slug]/layout.js
@@ -2,11 +2,12 @@ import HomeLink from "../HomeLink";
 
 export default function Layout({ children }) {
   return (
-    <>
+    <div className="relative">
+      {" "}
       {children}
       <footer className="mt-12">
         <HomeLink />
       </footer>
-    </>
+    </div>
   );
 }
--- app/[slug]/page.js
+++ app/[slug]/page.js
@@ -48,7 +48,7 @@ export default async function PostPage({ params }) {
           year: "numeric",
         })}
       </p>
-      <div className="markdown mt-10">
+      <div className="markdown mt-10 relative">
         <MDXRemote
           source={content}
           components={{
